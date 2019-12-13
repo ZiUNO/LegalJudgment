6126ad84b912daee2b58222098cc82aad468a124
ZiUNO <331245059@qq.com>
2019-12-13 10:06:08 +0800

    Add: exception.html:HTTP错误解决及传参

--- utils/web/flaskr/run.py
+++ utils/web/flaskr/run.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+import json
+
+from flask import Flask, render_template, request
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 
@@ -6,3 +9,19 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+
+
+@app.route('/search')
+def search():
+    q = request.args.get('q')
+    return q
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    exception = {
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    }
+    return render_template("exception.html", exception=exception)

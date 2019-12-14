8fbaa7bd15ceb9d0ed4a4ca66fd32a000215707e
ZiUNO <331245059@qq.com>
2019-12-14 10:49:39 +0800

    Need Fix: 高亮词汇应在用户所输入的文字上进行高亮，而不是在替换同义词后的文本上进行替换

--- main.py
+++ main.py
@@ -2,13 +2,17 @@
 import json
 import os
 
+from utils import merge
 from utils.engine.neo4j_engine import Neo4j
 
 if __name__ == '__main__':
     # NOTE:执行步骤：crawler.py->transform.py->main.py
-    with open("config.json", 'r', encoding='utf-8') as f:
-        config = json.load(f)
-    law_path = os.path.join(config['LAW_PATH'].replace('\\', os.path.sep))  # 格式化后的法律条文的保存路径
-    graph = Neo4j(law_path, rebuild=True)
-    graph.expand()
+    # with open("config.json", 'r', encoding='utf-8') as f:
+    #     config = json.load(f)
+    # law_path = os.path.join(config['LAW_PATH'].replace('\\', os.path.sep))  # 格式化后的法律条文的保存路径
+    # graph = Neo4j(law_path, rebuild=True)
+    # graph.expand()
+    result = [{"title": "a", "content": "b"}, {"title": "a", "content": "b"}, {"title": "c", "content": "d"}]
+    merged_result = merge(result)
+    print(merged_result)
 
--- utils/__init__.py
+++ utils/__init__.py
@@ -13,3 +13,24 @@ class MultiThread(Thread):
 
     def get_result(self):
         return self.__result
+
+
+def merge(result):
+    # TODO 归并结果，获取最终合并后的结果
+    result_len = len(result)
+    if result_len == 1:
+        return result
+    middle = result_len / 2
+    before_result = result[:middle]
+    after_result = result[middle:]
+    before_merged_result = merge(before_result)
+    after_merged_result = merge(after_result)
+    raw_result = before_merged_result + after_merged_result
+    if isinstance(raw_result[0], dict):
+        keys = list(raw_result[0].keys())
+        result_value = [tuple(value for value in raw_re[key]) for key in keys for raw_re in raw_result]
+        result_value = list(set(result_value))
+        input(result_value)
+    elif isinstance(raw_result[0], tuple):
+        merged_result = None
+    return merged_result

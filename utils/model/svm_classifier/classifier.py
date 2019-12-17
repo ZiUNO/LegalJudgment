878db4ac0efd7475a5db25271669442e83b8c2a7
ZiUNO <331245059@qq.com>
2019-12-17 13:49:04 +0800

    Failed: 回归问题正确率较低

--- utils/model/svm_classifier/classifier.py
+++ utils/model/svm_classifier/classifier.py
@@ -2,17 +2,22 @@ import csv
 import re
 import sys
 from datetime import datetime
+from time import time
 
 import joblib
 import numpy as np
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import SVC
+from sklearn.svm import SVC, SVR
 
 from utils.model.predict import Predict
 
 
-class SVMClassifier(object):
-    clf = None
+class SVM(object):
+    PREDS = {
+        "article": 1,
+        "imprisonment": 6,
+    }
+    model = None
     save_path = None
 
     @staticmethod
@@ -28,92 +33,127 @@ class SVMClassifier(object):
             return lines
 
     @staticmethod
-    def tsv2dataset(tsv):
+    def tsv2dataset(tsv, pred_type):
         datasets = {"X": [], "y": []}
         for i, line in enumerate(tsv):
             if i == 0:
                 continue
-            article = line[1][1:-1].split(',')
-            article = list(set([int(i.strip()) for i in article]))
-            article_ids = Predict.convert_article_to_ids(articles=article)
+            if pred_type == "article":
+                article = line[SVMClassifier.PREDS[pred_type]][1:-1].split(',')
+                article = list(set([int(i.strip()) for i in article]))
+                article_ids = Predict.convert_article_to_ids(articles=article)
+                datasets["y"].append(article_ids)
+            elif pred_type == "imprisonment":
+                death_penality = True if line[5] == "True" else False
+                life_imprisonment = True if line[7] == "True" else False
+                if death_penality or life_imprisonment:
+                    continue
+                imprison = float(
+                    line[SVM.PREDS[pred_type]]) + death_penality * 20.0 * 12 + life_imprisonment * 20.0 * 12
+                datasets["y"].append(imprison)
             charge = list(set(re.findall(u"'(.*?)'", line[2], re.S)))
             charge_ids = Predict.convert_charge_to_ids(charges=charge)
             datasets["X"].append(charge_ids)
-            datasets["y"].append(article_ids)
+
         datasets["X"] = np.array(datasets["X"])
         datasets["y"] = np.array(datasets["y"])
         return datasets
 
     @classmethod
-    def train(cls, train_datasets):
-        X_train, y_train = train_datasets
-        cls.clf = OneVsRestClassifier(SVC(gamma="auto"), n_jobs=-1) if cls.clf is None else cls.clf
-        cls.clf.fit(X_train, y_train)
-        return cls.clf
-
-    @classmethod
-    def load(cls, load_path=None, clf=None):
-        if clf is not None:
-            cls.clf = clf
+    def load(cls, load_path=None, model=None):
+        if model is not None:
+            cls.model = model
         elif load_path is not None:
-            cls.clf = joblib.load(load_path)
+            cls.model = joblib.load(load_path)
         else:
             raise ValueError("clf and load path should not be None at the same time")
-        return cls.clf
+        return cls.model
 
     @classmethod
-    def save(cls, clf=None, save_path=None):
+    def save(cls, model=None, save_path=None):
         if save_path is None:
             raise ValueError("save path should not be empty")
-        elif clf is not None:
-            joblib.dump(clf, save_path)
-            cls.clf = clf
-        elif cls.clf is None:
+        elif model is not None:
+            joblib.dump(model, save_path)
+            cls.model = model
+        elif cls.model is None:
             raise ValueError("cls.clf and clf should not be None at the same time")
         else:
-            joblib.dump(cls.clf, save_path)
+            joblib.dump(cls.model, save_path)
+
+    @classmethod
+    def train(cls, train_datasets):
+        raise NotImplementedError()
 
     @classmethod
     def predict(cls, X_test):
-        return cls.clf.predict(X_test)
+        return cls.model.predict(X_test)
 
     @classmethod
     def score(cls, X, y):
-        return cls.clf.score(X, y)
+        return cls.model.score(X, y)
+
+
+class SVMRegression(SVM):
+
+    @classmethod
+    def train(cls, train_datasets):
+        X_train, y_train = train_datasets
+        cls.model = SVR(kernel="linear") if cls.model is None else cls.model
+        cls.model.fit(X=X_train, y=y_train)
+        return cls.model
+
+
+class SVMClassifier(SVM):
+
+    @classmethod
+    def train(cls, train_datasets):
+        X_train, y_train = train_datasets
+        cls.model = OneVsRestClassifier(SVC(gamma="auto"), n_jobs=-1) if cls.model is None else cls.model
+        cls.model.fit(X_train, y_train)
+        return cls.model
 
 
 if __name__ == '__main__':
     path = r"E:/PyCharmWorkspace/LegalJudgment/utils/model/torch_pretrained_bert_multi_label/multi_data_dir/exercise_contest/"
-    datasets = {"train": "train.tsv", "test": "test.tsv", "val": "val.tsv"}
-    datasets = {key: path + datasets[key] for key in datasets}
-    datasets = {key: SVMClassifier.tsv2dataset(SVMClassifier.read_tsv(datasets[key])) for key in datasets}
-    X_train = datasets["train"]["X"]
-    y_train = datasets["train"]["y"]
-    X_test = datasets["test"]["X"]
-    y_test = datasets["test"]["y"]
-
-    pkl_path = "svm_clf.pkl"
+    # clf_datasets = {"train": "train.tsv", "test": "test.tsv", "val": "val.tsv"}
+    reg_datasets = {"train": "train.tsv", "test": "test.tsv", "val": "val.tsv"}
+    # clf_datasets = {key: path + clf_datasets[key] for key in clf_datasets}
+    reg_datasets = {key: path + reg_datasets[key] for key in reg_datasets}
+    # clf_datasets = {key: SVMClassifier.tsv2dataset(SVMClassifier.read_tsv(clf_datasets[key]), "charge") for key in
+    #                 clf_datasets}
+    reg_datasets = {key: SVM.tsv2dataset(SVM.read_tsv(
+        reg_datasets[key]), "imprisonment") for key in reg_datasets}
+    # X_train = clf_datasets["train"]["X"]
+    # y_train = clf_datasets["train"]["y"]
+    # X_test = clf_datasets["test"]["X"]
+    # y_test = clf_datasets["test"]["y"]
+    X_train = reg_datasets["train"]["X"]
+    y_train = reg_datasets["train"]["y"]
+    X_test = reg_datasets["test"]["X"]
+    y_test = reg_datasets["test"]["y"]
+    pkl_path = "svm_reg.pkl"
+    # pkl_path = "svm_clf.pkl"
 
     # train
-    # start_time = time()
-    # clf = SVMClassifier.train((X_train, y_train))
-    # print("train cost time: %d(s)" % (time() - start_time))
+    start_time = time()
+    # model = SVMClassifier.train((X_train, y_train))
+    model = SVMRegression.train((X_train, y_train))
+    print("train cost time: %.02f(s)" % (time() - start_time))
 
     # save
-    # SVMClassifier.save(save_path=pkl_path)
+    SVM.save(save_path=pkl_path, model=model)
 
     # load
-    start_time = datetime.now().microsecond
-    clf = SVMClassifier.load(pkl_path)
-    print("load cost time: %d(ms)" % (datetime.now().microsecond - start_time))
-
-    # save-2
-    SVMClassifier.save(save_path=pkl_path, clf=clf)
+    # start_time = datetime.now().microsecond
+    # model = SVM.load(load_path=pkl_path)
+    # print("load cost time: %d(ms)" % (datetime.now().microsecond - start_time))
 
     # predict
-    start_time = datetime.now().microsecond
-    y_pred_0 = SVMClassifier.predict(X_test[0:1])
-    print("predict once cost time: %d(ms)" % (datetime.now().microsecond - start_time))
+    start_time = time()
+    # y_pred_0 = SVMClassifier.predict(X_test[0:1])
+    y_pred_0 = SVM.predict(X_test[0:1])
+    print("predict once cost time: %.02f(s)" % (time() - start_time))
 
     # compare y_test[0:1] with y_pred_0
     print("-" * 10 + " y_pred_0 " + "-" * 10)
@@ -121,7 +161,7 @@ if __name__ == '__main__':
     print("-" * 10 + " y_test[0:1] " + "-" * 10)
     print(y_test[0:1])
 
-    # print("-" * 20)
-    # start_time = datetime.now().second
-    # print("score: %f" % clf.score(X_test, y_test))
-    # print("calculate score cost time: %d(s)" % (datetime.now().second - start_time))
+    print("-" * 20)
+    start_time = time()
+    print("score: %f" % SVM.score(X_test, y_test))
+    print("calculate score cost time: %f(s)" % (time() - start_time))

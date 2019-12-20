e78bd147671e9bf9c879f3012c775edc411b8ec2
ZiUNO <331245059@qq.com>
2019-12-20 21:19:24 +0800

    Modify: pages/index:输入框/确认按钮/相关按钮

--- sofa/pages/index/index.js
+++ sofa/pages/index/index.js
@@ -8,5 +8,34 @@ Page({
   },
   onLoad: function () {
     
+  },
+  handleInput: function (e) {
+    let input = e.detail.value
+    let lastChar = input.charAt(input.length - 1)
+    if (lastChar == '\n'){
+      this.search(e.detail.value)
+    }
+  },
+  search: function (e) {
+    let q = ''
+    if (e.constructor === String){
+      q = e
+    }
+    else{
+      q = e.detail.value
+    }
+    console.log(e)
+    // wx.request({
+    //   url: 'http://localhost:5000/search',
+    //   data: {
+    //     'q': e
+    //   },
+    //   success: function (result){
+    //     console.log(result)
+    //   }
+    // })
+  },
+  about: function (e) {
+    console.log(e)
   }
 })
